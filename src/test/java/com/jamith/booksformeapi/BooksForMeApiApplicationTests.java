4210e42fd7318b5d07aa267293acbfae1bdc606b
sheronfdo <jamithsheron5@gmail.com>
2025-02-01 23:09:24 +0530

    initial project for books for me API

---
+++ src/test/java/com/jamith/booksformeapi/BooksForMeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamith.booksformeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksForMeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
