3c39f7a707ce6a1afc50d72639af20025ae21fbc
sheronfdo <jamithsheron5@gmail.com>
2025-02-04 16:11:25 +0530

    firebase configurations done

---
+++ src/main/java/com/jamith/booksformeapi/config/FirebaseConfig.java
@@ -0,0 +1,24 @@
+package com.jamith.booksformeapi.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+    @PostConstruct
+    public void initializeFirebaseApp() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/books-for-me-firebase-adminsdk.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
