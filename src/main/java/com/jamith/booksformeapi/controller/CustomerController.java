f8e3f23708c4dc4f03515f0f7055c20e767c76f1
sheronfdo <jamithsheron5@gmail.com>
2025-02-11 15:31:30 +0530

    customer sign up process done

--- src/main/java/com/jamith/booksformeapi/controller/CustomerController.java
+++ src/main/java/com/jamith/booksformeapi/controller/CustomerController.java
@@ -1,15 +1,12 @@
 package com.jamith.booksformeapi.controller;
 
 import com.jamith.booksformeapi.dto.requestDTO.CustomerSignUpDTO;
-import com.jamith.booksformeapi.dto.requestDTO.SellerSignUpDTO;
 import com.jamith.booksformeapi.service.CustomerService;
-import com.jamith.booksformeapi.service.SellerService;
 import com.jamith.booksformeapi.utils.ResponseUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +30,4 @@ public class CustomerController {
             return ResponseUtil.generateErrorResponse("Unexpected error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
-
 }
